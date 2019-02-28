ad51c21ea35aecb68cd2b6ede1b63cdba831f939
YunJi Lee <tel4730@naver.com>
2019-02-28 07:06:43 +0000

    ready

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def telegram():
 
     
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0",port=8080)
+    app.run(debug=True,host=os.getenv("IP","0.0.0.0"),port=int(os.getenv("PORT",8080)))
